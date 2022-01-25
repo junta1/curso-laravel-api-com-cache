fbb6f681f9af34648024fd03a8b8f587de853f4c
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2022-01-24 21:15:38 -0300

    Adicionado estrutura Serveice Layer.

---
+++ app/Services/CourseService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\CourseRepository;
+
+class CourseService
+{
+    protected $repository;
+
+    public function __construct(CourseRepository $courseRepository)
+    {
+        $this->repository = $courseRepository;
+    }
+
+    public function getCourses()
+    {
+        return $this->repository->getAllCourses();
+    }
+}
+
+?>
